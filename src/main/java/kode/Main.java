38abecbc0441d623506d7fe6693824570d445590
Kazurov Kostya <kazurovzxc@mail.ru>
2025-06-25 18:09:08 +0200

    Добавить swagger

--- src/main/java/kode/Main.java
+++ src/main/java/kode/Main.java
@@ -1,5 +1,7 @@
 package kode;
 
+// Swagger UI: http://localhost:6432/swagger-ui.html
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
